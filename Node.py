348c00fa879a03ffb4f49eda1b66053445c60069
Avin Lad <avinlad@gmail.com>
2021-04-13 23:44:35 -0400

    final changes

--- Node.py
+++ Node.py
@@ -8,8 +8,6 @@ Email:   Bhin9310@mylaurier.ca, ladx7870@mylaurier.ca
 __updated__ = "2021-04-13"
 -------------------------------------------------------
 """
-
-from random import weibullvariate
 from common import *
 
 
@@ -18,23 +16,23 @@ class Node:
     def __init__(self, ID, networksimulator, costs):
         self.myID = ID
         self.ns = networksimulator
-        num = self.ns.NUM_NODES        
-        self.distanceTable = [[999 for i in range(num)] for j in range(num)]
-        self.routes = [0 for i in range(num)]
+        self.num = self.ns.NUM_NODES        
+        self.distanceTable = [[999 for i in range(self.num)] for j in range(self.num)]
+        self.routes = [0 for i in range(self.num)]
 
         # add id to route list for next hop
         self.routes[self.myID] = self.myID
 
         # sets values in distance table to their initial cost
-        for i in range(num):
+        for i in range(self.num):
             self.distanceTable[ID][i] = costs[i]
 
         # set the diagonal values of distance table to 0
-        for j in range(num):
+        for j in range(self.num):
            self.distanceTable[j][j] = 0
            
         # send a new packet with updated distance table
-        for k in range(num):
+        for k in range(self.num):
             if (self.routes[k] != 999) and (k != self.myID):
                 self.ns.tolayer2(RTPacket(self.myID, k, self.distanceTable[self.myID]))
 
@@ -44,7 +42,7 @@ class Node:
         # sets the mincosts to the distance table
         self.distanceTable[pkt.sourceid] = pkt.mincosts
 
-        for i in range(self.ns.NUM_NODES):
+        for i in range(self.num):
             if (pkt.mincosts[i] < self.distanceTable[pkt.sourceid][i]):
                 self.distanceTable[pkt.sourceid][i] = pkt.mincosts[i]
 
@@ -52,43 +50,26 @@ class Node:
         updated = False
 
         # bellman ford algorithm
-        for row in range(self.ns.NUM_NODES):
-            for col in range(self.ns.NUM_NODES):
+        for row in range(self.num):
+            for col in range(self.num):
                 if row != col:
                     if self.bellman_ford_algo(pkt, row, col):
                         updated = True
 
         # make sure the diagonal values remain 0 if not that means the values have updated
-        for diagonal in range(self.ns.NUM_NODES):
+        for diagonal in range(self.num):
             if self.distanceTable[diagonal][diagonal] != 0:
                 self.distanceTable[diagonal][diagonal] = 0
 
              
         if updated:
+            self.update_route()
             # sends the packet back to layer 2
-            self.routes[self.myID] = self.myID
-            for i in range(self.ns.NUM_NODES):
+            for i in range(self.num):
                 if self.routes[i] != 999 and i != self.myID:
                     self.ns.tolayer2(RTPacket(self.myID, i, self.distanceTable[self.myID]))
               
         return
-
-
-    def bellman_ford_algo(self, packet, source, destination):
-        updated = False
-        weight = []
-
-        # loop through adding the weights of neighbours
-        for i in range(self.ns.NUM_NODES):
-            weight.append(self.distanceTable[source][i] + self.distanceTable[i][destination])
-
-        # check if minimum is less than what is in distance table and replace if true
-        if (min(weight) < self.distanceTable[source][destination]):
-            self.distanceTable[source][destination] = min(weight)
-            updated = True        
-        
-        return updated
-    
 
 
     def printdt(self):
@@ -108,3 +89,27 @@ class Node:
             print()            
         print()
         
+
+    def bellman_ford_algo(self, pkt, source, destination):
+        updated = False
+        weight = []
+
+        # loop through adding the weights of neighbours
+        for i in range(self.num):
+            weight.append(self.distanceTable[source][i] + self.distanceTable[i][destination])
+
+        # check if minimum is less than what is in distance table and replace if true
+        if (min(weight) < self.distanceTable[source][destination]):
+            self.distanceTable[source][destination] = min(weight)
+            self.routes[pkt.sourceid] = weight.index(min(weight))
+            updated = True        
+        
+        return updated
+
+
+    def update_route(self):        
+
+        self.routes[self.myID] = self.myID
+
+        return 
+    
