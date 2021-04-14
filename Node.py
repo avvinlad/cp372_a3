0e266ae8af3a5e3997465e3e86998cb1d8916388
Avin Lad <avinlad@gmail.com>
2021-04-13 21:17:35 -0400

    distance tables are working

--- Node.py
+++ Node.py
@@ -9,6 +9,7 @@ __updated__ = "2021-04-13"
 -------------------------------------------------------
 """
 
+from random import weibullvariate
 from common import *
 
 
@@ -21,31 +22,75 @@ class Node:
         self.distanceTable = [[999 for i in range(num)] for j in range(num)]
         self.routes = [0 for i in range(num)]
 
-        # you implement the rest of constructor
+        # add id to route list for next hop
+        self.routes[self.myID] = self.myID
+
         # sets values in distance table to their initial cost
         for i in range(num):
             self.distanceTable[ID][i] = costs[i]
 
         # set the diagonal values of distance table to 0
-        for i in range(num):
-           self.distanceTable[i][i] = 0
+        for j in range(num):
+           self.distanceTable[j][j] = 0
+           
+        # send a new packet with updated distance table
+        for k in range(num):
+            if (self.routes[k] != 999) and (k != self.myID):
+                self.ns.tolayer2(RTPacket(self.myID, k, self.distanceTable[self.myID]))
 
-        # sets the values for the routes to the initial distance values
-        for i in range(len(self.distanceTable[self.myID])):
-            self.routes[i] = self.distanceTable[self.myID][i]
 
 
     def recvUpdate(self, pkt):
+        # sets the mincosts to the distance table
         self.distanceTable[pkt.sourceid] = pkt.mincosts
-        # you implement the rest of it 
-        
 
-        # sends the packet back to layer 2
-        self.ns.tolayer2(pkt)
+        for i in range(self.ns.NUM_NODES):
+            if (pkt.mincosts[i] < self.distanceTable[pkt.sourceid][i]):
+                self.distanceTable[pkt.sourceid][i] = pkt.mincosts[i]
 
+        # check for updated values in the distance table
+        updated = False
+
+        # bellman ford algorithm
+        for row in range(self.ns.NUM_NODES):
+            for col in range(self.ns.NUM_NODES):
+                if row != col:
+                    if self.bellman_ford_algo(pkt, row, col):
+                        updated = True
+
+        # make sure the diagonal values remain 0 if not that means the values have updated
+        for diagonal in range(self.ns.NUM_NODES):
+            if self.distanceTable[diagonal][diagonal] != 0:
+                self.distanceTable[diagonal][diagonal] = 0
+
+             
+        if updated:
+            # sends the packet back to layer 2
+            self.routes[self.myID] = self.myID
+            for i in range(self.ns.NUM_NODES):
+                if self.routes[i] != 999 and i != self.myID:
+                    self.ns.tolayer2(RTPacket(self.myID, i, self.distanceTable[self.myID]))
               
-        return 
+        return
+
+
+    def bellman_ford_algo(self, packet, source, destination):
+        updated = False
+        weight = []
+
+        # loop through adding the weights of neighbours
+        for i in range(self.ns.NUM_NODES):
+            weight.append(self.distanceTable[source][i] + self.distanceTable[i][destination])
+
+        # check if minimum is less than what is in distance table and replace if true
+        if (min(weight) < self.distanceTable[source][destination]):
+            self.distanceTable[source][destination] = min(weight)
+            updated = True        
+        
+        return updated
     
+
+
     def printdt(self):
         print("   D" + str(self.myID) + " |  ", end="")
         for i in range(self.ns.NUM_NODES):
